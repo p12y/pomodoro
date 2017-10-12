2e2fdc62b8e74db7bb4ae13c018e44f56f146b57
Peter Tyldesley <ptyldesley@schawk.com>
2017-10-12 11:01:36 +0100

    Set up react-md

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NavigationDrawer } from 'react-md';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <NavigationDrawer
+        drawerTitle="react-md with CRA"
+        toolbarTitle="Welcome to react-md"
+      >
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </NavigationDrawer>
     );
   }
 }
